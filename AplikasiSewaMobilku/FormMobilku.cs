0cbeda04669eda11e6996298474e751eaa2e36f8
andrianwibowo0@gmail.com <andrianwibowo0@gmail.com>
2021-12-25 08:35:32 +0700

    final projek netfirst commit

---
+++ AplikasiSewaMobilku/FormMobilku.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace AplikasiSewaMobilku
+{
+    public partial class FormMobilku : Form
+    {
+        String sql;
+        SqlCommand cmd;
+        SqlDataReader rd;
+
+        public FormMobilku()
+        {
+            InitializeComponent();
+            LoadDatamobilku();
+        }
+
+        public void LoadDatamobilku()
+        {
+            Koneksi.Open();
+
+            sql = " select * from MOBILKU order by nama mobil";
+            cmd = new SqlCommand(sql, Koneksi.conn);
+            rd = cmd.ExecuteReader();
+
+            if (rd.HasRows)
+            {
+                while (rd.Read())
+                {
+                    int n = dataGridView1.Rows.Add();
+                    dataGridView1.Rows[n].Cells[0].Value = rd["id_mobil"].ToString();
+                    dataGridView1.Rows[n].Cells[1].Value = rd["nama_mobil"].ToString();
+                    dataGridView1.Rows[n].Cells[2].Value = rd["id_mobil"].ToString();
+                    dataGridView1.Rows[n].Cells[3].Value = rd["tahun"].ToString();
+                    dataGridView1.Rows[n].Cells[4].Value = rd["rating"].ToString();
+                    dataGridView1.Rows[n].Cells[5].Value = rd["kategori"].ToString();
+                    dataGridView1.Rows[n].Cells[6].Value = rd["harga_sewa"].ToString();
+
+
+                }
+            }
+            rd.Close();
+            Koneksi.Close();
+            cmd.Dispose();
+        }
+    }
+}
---
+++ AplikasiSewaMobilku/Koneksi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace AplikasiSewaMobilku
+{
+    class Koneksi
+    {
+
+        private static String connString;
+        public static SqlConnection conn;
+
+        public static void Open()
+        {
+            connString = @"data source=.; initial catalog = SEWA_MOBILKU; "
+                + "integrated security = true";
+
+            conn = new SqlConnection(connString);
+
+            if (conn.State == System.Data.ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+        }
+
+        public static void Close()
+        {
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
+
+    }
+}
